ec653ad218fbc90d1787b98fbe96799c890b82cf
Maria Anofrieva <mariyas12@yandex.ru>
2023-05-12 17:51:25 +0400

    fix: server

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
+import type { Response, Request } from "express-serve-static-core";
+
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("./dist"));
+
+app.get("*", (_: Request, res: Response) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
